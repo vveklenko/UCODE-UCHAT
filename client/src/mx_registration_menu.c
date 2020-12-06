6ff88199d1189303bc8f8a257a95b74be9e51309
Vladislav Veklenko <vveklenko@c1r2p2.khpi.ucode-connect.study>
2020-12-06 14:35:52 +0200

    finiish

--- client/src/mx_registration_menu.c
+++ client/src/mx_registration_menu.c
@@ -17,15 +17,9 @@ void mx_registration_menu() {
     gtk_fixed_put(GTK_FIXED(black_background), registration_menu,
     CUR_WIDTH / 5 + 70, CUR_HEIGHT / 5);
 
-
+    //inscription Log in
     GtkWidget *log_in = gtk_label_new("Log in");
     gtk_box_pack_start(GTK_BOX(registration_menu), log_in, FALSE, FALSE, 0);
     gtk_widget_set_name(GTK_WIDGET(log_in), "Login");
-
-    GtkWidget *entry_username = gtk_entry_new();
-    gtk_widget_set_sensitive(GTK_WIDGET(*entry_username), FALSE);
-    gtk_widget_set_name(GTK_WIDGET(*entry_username), "entry_chat");
-    gtk_box_pack_start(GTK_BOX(chat_container), *entry_username, TRUE, TRUE, 8);
-    gtk_entry_set_placeholder_text(GTK_ENTRY(*entry_username), "Write a message...");
-    g_signal_connect(G_OBJECT(*entry_username), "changed", G_CALLBACK(entry_chat_fill_event), NULL);
+    
 }
